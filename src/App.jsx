082c286a7477aa190112a57e4a158a910bdbb77c
Ilya <ilya02989@gmail.com>
2024-12-03 11:34:59 +0300

    added first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
+import Card from "./components/Card"
+import Drawer from "./components/Drawer"
+import Header from "./components/Header"
 
 function App() {
   return <div className='wrapper clear'>
-    <header className="d-flex justify-between align-center p-40">
-      <div className="d-flex align-center">
-        <img width={40} height={40} src='/img/logo.png' />
-        <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кросовок</p>
+    <Drawer />
+    <Header />
+
+    <div className="content p-40">
+      <div className="d-flex align-center mb-40 justify-between">
+        <h1 >Все кросовки</h1>
+        <div className="search-block d-flex">
+          <img src="/img/search.svg" alt="search" />
+          <input type="text" placeholder="Поиск..." />
         </div>
       </div>
-      <ul className="d-flex">
-        <li className="mr-30">
-          <img width={18} height={18} src='/img/shopping-cart.svg' />
-          <span>1205 руб.</span>
-        </li>
-        <li>
-          <img width={18} height={18} src="/img/profile.svg" />
-        </li>
-      </ul>
-    </header>
-    <div className="content p-40">
-      <h1 className="mb-40">Все кросовки</h1>
+
+
       <div className="d-flex">
-        <div className="card">
-          <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-          <div className="d-flex justify-between align-center">
-            <div className="d-flex flex-column">
-              <span>Цена:</span>
-              <b>12 999 руб.</b>
-            </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" />
-            </button>
-          </div>
-        </div>
+        <Card />
         <div className="card">
           <img width={133} height={112} src="/img/sneakers/2.jpg" alt="sneakers" />
           <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
